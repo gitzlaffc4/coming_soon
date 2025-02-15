5618faa5091b7a17bd62e764d91611ba7d5de678
Cameron Gitzlaff <31455118+gitzlaffc4@users.noreply.github.com>
2025-02-15 13:32:29 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,20 +20,9 @@ const IndexPage = () => (
         </h3>
         <h1 className="text-7xl lg:text-9xl font-extrabold">Pardon the mess!</h1>
         <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
-          We`re under construction. Check back for an update soon. Stay in
-          touch!
+          We`re under construction. Check back for an update soon.
         </p>
       </div>
-      <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
-        <input
-          type="text"
-          placeholder="hi@themeptation.net"
-          className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
-        />
-        <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
-          Notify me
-        </button>
-      </form>
     </div>
   </Layout>
 );
